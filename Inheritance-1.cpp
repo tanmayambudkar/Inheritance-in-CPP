dc9933d08494201088b7206e934f55f9b21555b9
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-09-12 22:08:35 +0530

    Add files via upload

---
+++ Inheritance-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Animal {
+//base class
+public:
+    void eat() {
+        cout<<"This animal eats food."<< endl;
+    }
+};
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "The dog barks." << endl;
+    }
+};
+int main() {
+    Dog myDog;
+    myDog.eat();   
+    myDog.bark();  
+    return 0;
+}
